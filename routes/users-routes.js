914ca3470da9e399d8f396fec56f1db0b87da0ca
Oussama Jamal <ouss03zam@gmail.com>
2026-01-01 23:59:56 +0200

    Added routes for users

---
+++ routes/users-routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const usersController = require("../controllers/users-controllers");
+
+const router = express.Router();
+
+router.get("/", usersController.getUsers);
+
+router.post("/login", usersController.login);
+
+router.post("/signup",usersController.signup);
+
+
+module.exports = router;

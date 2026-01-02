9a44e016e4faf44e8cf569aaabb0c89eb6892a7b
Oussama Jamal <ouss03zam@gmail.com>
2026-01-02 20:14:33 +0200

    added a post api signup with bcrypt to hash the user plain password

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -40,24 +40,7 @@ const signup = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
-  const q = "SELECT * FROM users WHERE email = ? AND password = ?";
-  db.query(q, [email, password], (err, data) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({
-        error: "Failed to login",
-      });
-    }
-    if (data.length === 0) {
-      return res.status(401).json({
-        error: "Invalid email or password",
-      });
-    }
-    return res.status(200).json({
-      message: "Login successful",
-    });
-  });
+
 };
 
 exports.getUsers = getUsers;
